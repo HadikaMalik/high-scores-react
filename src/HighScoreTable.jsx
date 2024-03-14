7cc91c705b90073f76c17fab00a46cb8ec093d73
Your Name <you@example.com>
2024-03-14 14:17:22 +0000

    sorted scores in descending order

--- src/HighScoreTable.jsx
+++ src/HighScoreTable.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 const HighScoreTable = ({ country }) => {
+    const sortedScores = [...country.scores].sort((a,b) => b.s - a.s)
     return (
         <>
             <div className="box">
                 <h2 className="sub-heading">High scores: {country.name}</h2>
                 <table>
                     <tbody>
-                        {country.scores.map((score, index) => {
+                        {sortedScores.map((score, index) => {
                             return (
                                 <tr key={index}>
                                     <td>{score.n.toUpperCase()}</td>
