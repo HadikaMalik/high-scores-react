6c7e93ec28623ccf94e025bc42f18dc17609ee90
Your Name <you@example.com>
2024-03-14 14:56:36 +0000

    button and styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import allCountryScores from "./scores";
 import HighScoreTable from "./HighScoreTable";
 import './App.css';
@@ -8,14 +8,16 @@ function App() {
   const toggleSortOrder = () => {
     setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
   };
-  const sortedCountryNames = [...allCountryScores].sort((a,b) => a.name.localeCompare(b.name));
+  const sortedCountryNames = [...allCountryScores].sort((a, b) => a.name.localeCompare(b.name));
   return (
     <div className="App">
-      <button onClick={toggleSortOrder}>Sort scores by asc/desc</button>
-    <h1 className="heading">High Scores per Country</h1>
-      {sortedCountryNames.map((country, index) => (
-        <HighScoreTable key={index} country={country} sortOrder={sortOrder}/>
-      ))}
+      <button className="toggle-button" onClick={toggleSortOrder}>Sort scores by asc/desc</button>
+      <div className="container">
+        <h1 className="heading">High Scores per Country</h1>
+        {sortedCountryNames.map((country, index) => (
+          <HighScoreTable key={index} country={country} sortOrder={sortOrder} />
+        ))}
+      </div>
     </div>
   );
 }
